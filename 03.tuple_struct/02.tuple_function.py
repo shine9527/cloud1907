9af486c20f6914db31feccdb0f1435c953e52ea9
bavdu <bavduer@163.com>
2019-09-25 11:48:14 +0800

    example

---
+++ 03.tuple_struct/02.tuple_function.py
@@ -0,0 +1,25 @@
+# 元组中自带的一些方法
+# t01 = (1, 2, 3, 4, 5)
+#
+# index = t01.index(3)
+# print(index)
+#
+# search = int(input('please input your search: '))
+# if search in t01:
+#     print("{}'s index: {}".format(search, t01.index(search)))
+# else:
+#     print("{} not exist of t01.".format(search))
+
+# t02 = (1, 2, 3, 4, 5, 1, 2, 3)
+# print(t02.index(1, 1, len(t02)-1))
+
+# t03 = (1, 2, 1, 3, 4, 5, 1, 2, 3, 4, 5)
+# number = t03.count(1)
+# print(number)
+
+# t04 = (12, 7)
+# t05 = (11, 5)
+# if t04 > t05:
+#     print(True)
+# else:
+#     print(False)
---
+++ 03.tuple_struct/03.example01.py
@@ -0,0 +1,25 @@
+# 现在让用户输入自己的出生年份，再输入出生的日期
+# 打印出该用户的属相，输出用户的星座
+
+
+# sx = '猴鸡狗猪鼠牛虎兔龙蛇马羊'
+# year = int(input("please input your year: "))
+# print(sx[year % 12])
+
+xz = ('水瓶座', '双鱼座', '白羊座', '金牛座', '双子座', '巨蟹座',
+      '狮子座', '处女座', '天秤座', '天蝎座', '射手座', '摩羯座')
+rq = ((1, 20), (2, 19), (3, 21), (4, 20), (5, 21), (6, 22),
+      (7, 23), (8, 23), (9, 23), (10, 24), (11, 23), (12, 22))
+month = int(input("please input your month: "))
+date = int(input("please input your date: "))
+
+for dates in range(len(rq)):
+    if (month, date) >= (12, 22):
+        print(xz[-1])
+        break
+    elif (month, date) >= rq[dates]:
+        continue
+    else:
+        print(xz[dates - 1])
+        break
+
