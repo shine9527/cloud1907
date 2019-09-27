4daccb8408542bd358ffee7575a1e128ad4ae5e7
bavdu <bavduer@163.com>
2019-09-27 10:11:40 +0800

    tkings

--- 05.file_opera/tkings_find_character.py
+++ 05.file_opera/tkings_find_character.py
@@ -2,15 +2,15 @@
 # 并用字典存储, 形式为: {人物01：次数, 人物02：次数, ...}
 
 file = open(file='./file/tkings.txt', mode='r', encoding='utf8')
-
 content = file.read().replace('\n', '')
-
 character = {}
 user = ('关羽', '曹操', '刘备', '张飞', '赵云')
-
 for i in user:
     if i not in character.keys():
         character.setdefault(i, content.count(i))
 print(character)
-
 file.close()
+
+# 请统计出tkings.txt中, 『青龙偃月刀』、『丈八蛇矛』、『雌雄双股剑』、『青釭剑』、『方天画戟』
+# 出现的次数，用字典存储
+
